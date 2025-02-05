4c043efa39a5896eca7bea9f54cc815d89874f8b
Wildrey237 <wilfriedbemelingue@gmail.com>
2025-02-06 00:46:47 +0100

    work

--- view/send_message.php
+++ view/send_message.php
@@ -28,40 +28,35 @@ $messages = recuperer_messages($user_id);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <title>Chatbox</title>
+    <title>Messagerie - CrewConnect</title>
+    <link rel="stylesheet" href="../style/navbar.css">
+    <link rel="stylesheet" href="../style/page_accueil.css">
 </head>
 <body>
-<nav>
-    <p class="logo-upper p-3 m-0 border-0 bd-example text-light">WERJ</p>
-</nav>
-
-<h1 class="text-center">Messagerie</h1><br>
-<div class="container mt-5">
-    <div class="row">
-        <div class="col-sm-5 m-auto">
-            <div class="card">
-                <div class="card-body">
-                    <div class="form-group mb-3">
-                        <?php foreach ($messages as $message): ?>
-                            <div class="message">
-                                <?php
-                                $sender_name = get_user_name_by_id($message['user_user_id']);
-                                ?>
-                                <p><strong><?php echo htmlspecialchars($sender_name); ?>
-                                        :</strong> <?php echo htmlspecialchars($message['content']); ?></p>
-                                <p class="date"><?php echo $message['sent_date']; ?></p>
-                            </div>
-                        <?php endforeach; ?>
-                        <form action="send_message.php?id_user=<?php echo $id_receveur; ?>" method="post">
-                            <input type="hidden" name="id_receveur" value="<?php echo $id_receveur; ?>">
-                            <input type="text" name="content" placeholder="Entrer votre message" required>
-                            <input type="submit" value="Envoyer">
-                        </form>
+<?php include_once("navbar.php"); ?>
+<div class="main">
+    <div class="logo">
+        <img src="../images/CC Fond BLANC_modifié.jpg" alt="Logo CrewConnect">
+    </div>
+    <div class="search__container">
+        <div class="messages-content">
+            <h2>Messagerie</h2>
+            <div class="message-list">
+                <?php foreach ($messages as $message): ?>
+                    <div class="message">
+                        <?php
+                        $sender_name = get_user_name_by_id($message['user_user_id']);
+                        ?>
+                        <p><strong><?php echo htmlspecialchars($sender_name); ?>:</strong> <?php echo htmlspecialchars($message['content']); ?></p>
+                        <p class="date"><?php echo $message['sent_date']; ?></p>
                     </div>
-                </div>
+                <?php endforeach; ?>
             </div>
+            <form action="send_message.php?id_user=<?php echo $id_receveur; ?>" method="post" class="message-form">
+                <input type="hidden" name="id_receveur" value="<?php echo $id_receveur; ?>">
+                <input type="text" name="content" placeholder="Entrer votre message" required>
+                <input type="submit" value="Envoyer">
+            </form>
         </div>
     </div>
 </div>
